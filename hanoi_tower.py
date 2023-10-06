611961af8821773bd8b619e9ec096cc4e3f183ca
lucasqr <lucasq.rodrigues15@gmail.com>
2023-10-06 20:00:31 -0300

    message

---
+++ hanoi_tower.py
@@ -0,0 +1,49 @@
+import pilha 
+
+def criar(num:int):
+    pino = pilha.pilha(num)
+    
+    for i in range(num):
+        pino.empilha(num-i)
+    return pino
+
+def mover(A:pilha.pilha, B:pilha.pilha):
+     B.empilha(A.data[-1])
+     A.desempilha()
+
+class hanoi:
+    def __init__(self, num:int):
+        self.num = num
+        self.inicial = criar(num) 
+        self.auxiliar = pilha.pilha(num)
+        self.final = pilha.pilha(num)
+        self.passos =  0
+
+    def mov_disc(self, mov:int):
+        if mov != 0 and self.passos % mov == 0 and self.passos != 2 ** self.num - 1:
+            print(self.inicial.data.tolist(), "pino inicial\n", self.auxiliar.data.tolist(), "pino auxiliar\n", self.final.data.tolist(), "pino final\n", "Passos =", self.passos)
+            input("favor pressionar [ENTER] para continuar")
+            return
+        else:
+            return
+
+    def resolver(self, Mov = 0 ):    
+        print(self.inicial.data.tolist(), "pino inicial\n", self.auxiliar.data.tolist(), "pino auxiliar\n", self.final.data.tolist(), "pino final\n", "Passos =", self.passos)
+        input("favor pressionar [ENTER] para continuar")
+        def resolver_inter(M, n = self.num, A = self.inicial, B= self.auxiliar, C=self.final):
+            if n == 1:
+                mover(A, C)
+                self.passos += 1
+                self.mov_disc(M)
+            else:
+                resolver_inter(M, n-1, A, C, B)
+                mover(A, C)
+                self.passos += 1
+                self.mov_disc(M)
+                resolver_inter(M, n-1, B, A, C)
+        resolver_inter(Mov)
+        print(self.inicial.data.tolist(), "pino inicial\n", self.auxiliar.data.tolist(), "pino auxiliar\n", self.final.data.tolist(), "pino final\n", "Passos =", self.passos)
+        return
+    
+teste = hanoi(7)
+teste.resolver(10)
---
+++ pilha.py
@@ -0,0 +1,47 @@
+import array
+
+class pilha:
+    def __init__(self, max:int = 100, tipo = 'f'):
+        self.data = array.array(tipo)
+        self.max = max
+        self.len= 0
+        
+
+    def pilha_esta_cheia(self):
+        return self.len == self.max
+    
+    def pilha_esta_vazia(self):
+        return self.len == 0 
+    
+    def tamanho(self):
+        return self.len
+    
+    def empilha(self, item):
+        if self.len == self.max:
+            raise Exception("PilhaCheiaErro")
+        
+        else:
+            try:
+                self.data.append(item)
+                self.len += 1
+            except:
+                raise Exception("TipoErro")
+    
+    def desempilha(self):
+        if self.len == 0:
+            raise Exception("PilhaVaziaErro")
+        
+        else:
+            self.data.pop()
+            self.len += -1
+
+    def troca(self):
+        if self.len <2:
+            print("requer ao menos dois elementos")
+        
+        else:
+            ultimo = self.data.pop()
+            penultimo = self.data.pop()
+            self.data.append(ultimo)
+            self.data.append(penultimo)
+
